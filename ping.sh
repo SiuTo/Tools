9608b7c837d99aacc7d04fcab8a257b1ed79dc43
Xiaotao Liang <418xiaotao@163.com>
2017-01-01 23:19:08 +0800

    fix: discard error messages

--- ping.sh
+++ ping.sh
@@ -1,9 +1,11 @@
-#! /bin/bash
+#! /bin/sh
+
 for ip in `cat $1`; do
-	avgtime=`ping -c 3 -W 1 $ip 2>null | grep rtt | awk -F / '{ print $5 }'`
+	avgtime=`ping -c 3 -W 1 $ip 2>/dev/null | grep rtt | awk -F / '{ print $5 }'`
 	if test -z $avgtime; then
 		printf '%s    no\n' $ip
 	else
 		printf '%s    %s ms\n' $ip $avgtime
 	fi
 done
+
